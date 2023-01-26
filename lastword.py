2ec50000e740d848c5f1132d0a76df0ea7304e56
MisterSerious <43838750+ASeriousMister@users.noreply.github.com>
2023-01-26 19:17:38 +0000

    Update lastword.py
    Improved generation of random bits to complete entropy
    Fixed issue when choosing first word of wordlist

--- lastword.py
+++ lastword.py
@@ -4,6 +4,8 @@ import hashlib
 import binascii
 import random
 
+system_random = random.SystemRandom()
+
 class color:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
@@ -15,6 +17,7 @@ class color:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
+
 
 # Converts string with 0 and 1 to hexadecimal
 def binToHexa(n):
@@ -54,15 +57,17 @@ def binToHexa(n):
         i = i - 1
     return hexaDeciNum[i]
 
+
 print(color.YELLOW + 'Welcome to lastword.py!' + color.END)
 print('Select the first words of your mnemonic with your personal method')
 print('This tool calculates a possible last word that respects the BIP39 standard')
 print(color.RED + 'DISCLAIMER: ' + color.END + 'always check that seeds are valid!\n')
 
-#select seed lenght
+# select seed lenght
 tour = True
 while tour:
-    s_len = input(color.DARKCYAN + 'How many words do you want your mnemonic seed to have? (12/15/18/21/24)\n' + color.END)
+    s_len = input(
+        color.DARKCYAN + 'How many words do you want your mnemonic seed to have? (12/15/18/21/24)\n' + color.END)
     if s_len.isnumeric():
         s_len = int(s_len)
         if (s_len == 12 or s_len == 15 or s_len == 18 or s_len == 21 or s_len == 24):
@@ -70,8 +75,8 @@ while tour:
             tour = False
         else:
             print(color.RED + f'{s_len} not accepted! Please chose between 12, 15, 18, 21 or 24 words' + color.END)
- 
- #select seed language
+
+# select seed language
 tour2 = True
 while tour2:
     print(color.DARKCYAN + 'Select the language for your mnemonic seed, typing the corresponding number' + color.END)
@@ -105,9 +110,9 @@ while tour2:
             print(color.RED + 'Unallowed number, only numbers between 1 and 10 are allowed' + color.END)
     else:
         print(color.RED + 'Unallowed answer, only numbers between 1 and 10 are allowed' + color.END)
-         
+
 # user provides words
-seed_str = '' # stores the first words of the seed
+seed_str = ''  # stores the first words of the seed
 str_bin = ''
 wordlist = wl.read()
 i = 0
@@ -120,13 +125,25 @@ while i < (s_len - 1):
         for num, line in enumerate(wl, 1):
             if word in line:
                 index_dec = num - 1
-#        print(f'index: {index_dec}')
+                print(color.YELLOW + 'index: ' + color.END + f'{index_dec}')
         bin_word = str(bin(index_dec)[2:])
         bin_word = bin_word.zfill(11)
         str_bin += bin_word
+    elif (word + '\n') in wordlist:
+        wl.seek(0)
+        for num, line in enumerate(wl, 1):
+            if word in line:
+                index_dec = num - 1
+        if index_dec == 0:
+            print(color.YELLOW + 'index: ' + color.END + f'{index_dec}')
+            i += 1
+            seed_str = seed_str + word + ' '
+            bin_word = str(bin(index_dec)[2:])
+            bin_word = bin_word.zfill(11)
+            str_bin += bin_word
     else:
         print(color.RED + 'Unallowrd word!' + color.END)
-        
+
 # number of bits of entropy corresponding to each mnemonic seed lenght
 ent = 256
 if s_len == 12:
@@ -137,12 +154,13 @@ elif s_len == 18:
     ent = 192
 elif s_len == 21:
     ent = 224
- 
+
 # Fill entropy
-max_rand = 2**(ent-(11*(s_len - 1))) # each word corresponds to 11 bits. Some bits missing to reach the requested entropy to calculate checksum
-ran_bits = str(bin(random.randint(0, max_rand))[2:]) # random bits to add to reach entropy bits
+max_rand = 2 ** (ent - (11 * (s_len - 1)))  # each word corresponds to 11 bits. Some bits missing to reach the requested entropy to calculate checksum
+ran_bits = str(bin(system_random.randint(0, max_rand))[2:])  # random bits to add to reach entropy bits
 ran_bits = ran_bits.zfill(ent - (11 * (s_len - 1)))
-str_bin += ran_bits # adds last bits before the checksum
+print(color.YELLOW + '\nRandom bits added: ' + color.END + f'{ran_bits}\n')
+str_bin += ran_bits  # adds last bits before the checksum
 
 bytes = int(ent / 8)
 tmp_bin = str_bin
@@ -150,38 +168,38 @@ tmp_bin = str_bin
 bin_list = []
 start = 0
 part = 4
-while start < len(tmp_bin): # Splitting string in 4 digits parts
+while start < len(tmp_bin):  # Splitting string in 4 digits parts
     bin_list.append(tmp_bin[start: start + part])
     start += part
 # convert list with four 0 and 1 digits to list with hexadecimal letters
 hex_list = []
 for bn in bin_list:
     hex_list.append(binToHexa(bn))
-hex_ent = ''.join(hex_list)    # creates hexadecimal string of entropy
+hex_ent = ''.join(hex_list)  # creates hexadecimal string of entropy
 
 tmp_bin = binascii.unhexlify(hex_ent)  # binary of entropy
-tmp_hex = binascii.hexlify(tmp_bin)   # hexadecimal of entropy
+tmp_hex = binascii.hexlify(tmp_bin)  # hexadecimal of entropy
 
-str_hash = hashlib.sha256(tmp_bin).hexdigest()   # hashing binary of entropy
+str_hash = hashlib.sha256(tmp_bin).hexdigest()  # hashing binary of entropy
 
 # Converting hash to binary
 int_hash = int(str_hash, base=16)
 bin_hash = str(bin(int_hash))[2:]
 
-checksum_lenght = int(ent / 32) # Length of checksum is 1 bit every 32 of the mnemonic
+checksum_lenght = int(ent / 32)  # Length of checksum is 1 bit every 32 of the mnemonic
 checksum = bin_hash[0:checksum_lenght]  # Getting first digits of hash (4 to 8 depending on entropy)
 # Adding checksum to entropy
 binary_seed = (bin(int(tmp_hex, 16))[2:].zfill(bytes * 8) + bin(int(str_hash, 16))[2:].zfill(256)[: bytes * 8 // 32])
 
 # Finding the last word
-last_word_bin = binary_seed[-11:] # isolate the last 11 bits (random bits + checksum)
+last_word_bin = binary_seed[-11:]  # isolate the last 11 bits (random bits + checksum)
 last_word_index = int(last_word_bin, 2)
 wl.seek(0)
-for i, line in enumerate(wl): # access wordlist at the desired index
+for i, line in enumerate(wl):  # access wordlist at the desired index
     if i == last_word_index:
         last_word = line.strip('\n')
- 
-mnemonic = seed_str + last_word #add last word to mnemonic
+
+mnemonic = seed_str + last_word  # add last word to mnemonic
 print(color.GREEN + '\nYour mnemonic:' + color.END)
 print(color.DARKCYAN + mnemonic + color.END)
 
